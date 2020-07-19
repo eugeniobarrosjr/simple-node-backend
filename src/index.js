3604526a5b306342e92378d6c1f1ee9c5a19bff4
Eugenio Barros <eugenio.barros.jr@gmail.com>
2020-07-19 16:06:59 -0300

    feat: Study Query , Params, Body

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+
 const projects = [{id: 1, name: 'Project 1'},
 {id: 2, name: 'Project 2'}];
 
 app.get('/projects', (request, response) => {
+  const {title, owner} = request.query;
+  console.log(title, owner);
   return response.json(projects);
 });
 
 app.post('/projects', (request, response) => {
+  const project = request.body;
+  console.log(project);
   return response.json([...projects, {id: 3, name: 'Project 3'}]);
 });
 
 app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  console.log(id);
   return response.json([...projects, {id: 3, name: 'Project 4'}])
 });
 
