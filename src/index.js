6909ef5ba53b21b74ad6f21d07159131061fc19c
Eugenio Barros <eugenio.barros.jr@gmail.com>
2020-07-19 14:32:40 -0300

    feat: Study Routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 
+const projects = [{id: 1, name: 'Project 1'},
+{id: 2, name: 'Project 2'}];
+
 app.get('/projects', (request, response) => {
-  return response.json({message: 'Hello Nodemon'});
+  return response.json(projects);
+});
+
+app.post('/projects', (request, response) => {
+  return response.json([...projects, {id: 3, name: 'Project 3'}]);
+});
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([...projects, {id: 3, name: 'Project 4'}])
+});
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json({id: 3, name: 'Project 4'})
 });
 
 app.listen(3333, () => {
